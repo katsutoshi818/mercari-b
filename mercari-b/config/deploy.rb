7585699359b1c21e22df9d02fd19c78ebc916eb6
katsutoshi818 <katsutoshi8988@gmail.com>
2019-06-23 15:26:48 +0900

    微修正

--- mercari-b/config/deploy.rb
+++ mercari-b/config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/mercari.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: ['~/.ssh/mercari.pem']  
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
