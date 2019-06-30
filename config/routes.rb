b6fa081d00b034aa59c623f33dc3aeaf2f8e363d
katsutoshi818 <katsutoshi8988@gmail.com>
2019-06-30 12:51:27 +0900

    collectionを使用してルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :users, only: [:index]
-  get 'users/profiles' => 'profiles#index'
+  resources :users, only: [:index] do
+    collection do
+      get :profile
+    end
+  end
+  
 end
