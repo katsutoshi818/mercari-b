1720b2a5d9c6bdcbc556392a2df4c55a38e7e3ec
Akito <kuroya1993@gmail.com>
2019-07-04 18:28:54 +0900

    jsのデバッグ用記述を消去

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,5 +1,4 @@
 $(document).on('turbolinks:load', function() {
-  console.log('turbo!');
 
   $('.image-form__file').on('change', function() {
     console.log('change!');
@@ -14,16 +13,13 @@ $(document).on('turbolinks:load', function() {
     <select class="form-block__form--select" id="${hierarchy}_category" name="product[${hierarchy}_category_id]">
     <option value="0">---</option>`;
 
-    //引数の数だけoptionを追加する
     var option = ``;
     $.each(categories, function(i, category) {
       option += `<option value="${category.id}">${category.name}</option>`
-      //jbuilderでcategory.nameで名前が表示できるようにする？
     })
     html += option;
     html += `</select>
     <i class="fas fa-chevron-down select-arrow-append"></i>`;
-    // console.log('html ->' + html)
     categoryList.append(html);
   }
 
@@ -37,7 +33,6 @@ $(document).on('turbolinks:load', function() {
       $('#low_category').remove();
     }
 
-    console.log('click')
     $('#top_category').on('change', function() {
       var value = $(this).val();
       console.log("category_change");
@@ -49,20 +44,13 @@ $(document).on('turbolinks:load', function() {
           dataType: 'json'
         })
         .done(function(midCategories) {
-          midCategories.forEach(function(category) {
-            console.log(category.id);
-            console.log(category.name);
-          })
-          //!中カテゴリーのフォームをアペンドする。
-          //追加すべき内容を引数にしてappendCategoryFormを呼ぶ。
+          deleteMidCategories();
+          deleteLowCategories();
           if (midCategories.length !== 0) {
-            deleteMidCategories();
-            deleteLowCategories();
             appendCategoryForm(midCategories, 'mid');
           }
         })
         .fail(function() {
-          //fail
         })
       }
     })
@@ -79,15 +67,12 @@ $(document).on('turbolinks:load', function() {
           dataType: 'json'
         })
         .done(function(lowCategories) {
-          //!中カテゴリーのフォームをアペンドする。
-          //追加すべき内容を引数にしてappendCategoryFormを呼ぶ。
+          deleteLowCategories();
           if (lowCategories.length !== 0) {
-            deleteLowCategories();
             appendCategoryForm(lowCategories, 'low');
           }
         })
         .fail(function() {
-
         })
       }
     })
@@ -95,30 +80,3 @@ $(document).on('turbolinks:load', function() {
 
   });
 });
-
-//topカテゴリー選択時、すでにmid,lowカテゴリーがあればそれを消去してからアペンドする。
-//midカテゴリー選択時、すでにlowカテゴリーがあればそれを消去してからアペンドする。
-
-// （カテゴリ）実装すべき内容
-// function1 () {
-// 大カテゴリで選択されたことを確認
-// 子となっている中カテゴリの中身を取得(ajax)
-// 中カテゴリ選択用のselectフォームをappendする
-// }
-// function2 () {
-// 中カテゴリが選択されたことを確認
-// 子となっている小カテゴリを取得(ajax)
-// 子カテゴリ選択用のselectフォームをappendする。
-// }
-
-// （利益計算）実装すべき内容
-// function () {
-// 入力された価格を取得
-// 手数料と利益を計算
-// 手数料、利益を表示する。
-// //299円以下はだめ
-// }
-
-// （画像フィールド）実装すべき内容
-// 1.ファイルが追加されたのを確認
-// 2.ファイルの
