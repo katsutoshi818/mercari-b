d0f6a9df88d63d646b37b7b812dbbe664778878b
syukihiro <kaui.yukihiro@gmail.com>
2019-07-05 20:47:15 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resources :products, only: [:index, :new, :show, :edit, :update]
+  resources :products, only: [:index, :new, :show, :edit, :update, :create]
   resources :categories, only: [:index]
   resources :users, only: [:index] do
     collection do
