3734a9b8ac23e46f2b6918529661e0c79a4420ea
katsutoshi818 <katsutoshi8988@gmail.com>
2019-07-05 14:20:59 +0900

    カテゴリーのJSを動かす

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products, only: [:index, :new, :show, :edit, :update, :create]
+  resources :categories, only: [:index]
   resources :users, only: [:index] do
     collection do
       get :profile
