a566444fbdd5689498d58f1417df5524c5f63480
Akito <kuroya1993@gmail.com>
2019-07-06 20:21:50 +0900

    unless分岐をリファクタリング

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -6,10 +6,8 @@ class Brand < ApplicationRecord
   def brand_registration(brand_input)
     # その名前のブランドが存在するか調べる。
     brand = Brand.find_by('brand_name LIKE(?)', brand_input[:brand_name])
-    unless brand
-      # 入力されたブランドが存在しない場合、テーブルにブランドを登録する
-      brand = Brand.create(brand_name: brand_input[:brand_name], category_id: brand_input[:category_id])
-    end
+    # 入力されたブランドが存在しない場合、テーブルにブランドを登録する
+    brand ||= Brand.create(brand_name: brand_input[:brand_name], category_id: brand_input[:category_id])
     return brand[:id]
   end
 end
