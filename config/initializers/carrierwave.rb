87c61e70106de2fd55fd12e7691d3cf52987d6e9
katsutoshi818 <katsutoshi8988@gmail.com>
2019-07-07 21:24:29 +0900

    画像のアップロード先の詳細設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
+  if Rails.env.development? || Rails.env.test?
+    config.storage = :file
+  else
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region: 'ap-northeast-1'
   }
   config.storage = :fog
-  config.fog_provider = 'fog/aws'
 
   config.fog_directory  = 'mercari-b'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari-b'
