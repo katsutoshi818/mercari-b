f2cd255de2331206855c333ba5b90449b216a46e
katsutoshi818 <katsutoshi8988@gmail.com>
2019-07-07 21:43:13 +0900

    シンタックスエラーを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,8 +14,9 @@ CarrierWave.configure do |config|
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
-  }
-  config.storage = :fog
+    }
+    config.storage = :fog
+  end
 
   config.fog_directory  = 'mercari-b'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari-b'
