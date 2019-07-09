a014a9eddad9e88f6e91c1b125b00cfb1e6f2d94
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2019-07-09 17:50:29 +0900

    enumを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,8 +12,14 @@ class Product < ApplicationRecord
   validates :introduction, presence: true
   validates :product_state, presence: true
   validates :who_pays_shipping_fee, presence: true
-  validates :seller_prefecture, presence: true
+  validates :prefecture_id, presence: true
   validates :days_to_ship, presence: true
   validates :price, presence: true
   validates :trade_state, presence: true
+
+  enum product_state: [:"商品の状態を選択してください", :"とても良い", :"良い", :"普通", :"悪い", :"とても悪い"]
+  enum who_pays_shipping_fee: [:"送料負担者を選択してください", :"発送者負担", :"購入者負担"]
+  enum days_to_ship: [:"発送日数を選択してください", :"1〜2日で発送", :"3〜4日で発送", :"1週間以内に発送"]
+  enum trade_state: [:"trade", :"buying", :"stopping", :"buyed"]
+  enum way_to_ship: [:"発送方法を選択してください", :"らくらくメルカリ便", :"ゆうゆうメルカリ便", :"一般運送会社"]
 end
