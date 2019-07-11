a67dfaaa2a4af6123cd05738b8b6d9a99af66cb1
syukihiro <kaui.yukihiro@gmail.com>
2019-07-11 16:52:25 +0900

    edit-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'users/thanks' => 'users/registrations#thanks'
+  end
+
   root 'products#index'
   resources :products, only: [:index, :new, :show, :edit, :update, :create]
   resources :categories, only: [:index]
-  resources :users, only: [:index, :new] do
+  resources :users, only: [:show, :new] do
     collection do
       get :profile
       get :logout
