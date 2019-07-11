f1b3fa48507c438f95ec2d2905e210ef6ccee149
Akito <kuroya1993@gmail.com>
2019-07-11 13:32:29 +0900

    product_sizesコントローラー

---
+++ app/controllers/product_sizes_controller.rb
@@ -0,0 +1,9 @@
+class ProductSizesController < ApplicationController
+  def index
+    # paramsのsize_typeに基づいて、sizeテーブルからsize_typeが一致するレコードを取得する。
+    category_type = Category.find(params[:low_category])[:size_type]
+    respond_to do |format|
+      format.json { @sizes = ProductSize.where("size_type LIKE(?)", category_type) }
+    end
+  end
+end
