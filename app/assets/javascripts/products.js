28cab34302734eadb30f20cd2ab492cfe00152a7
Akito <kuroya1993@gmail.com>
2019-07-11 13:31:45 +0900

    サイズ機能JS

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,4 +1,6 @@
 $(document).on('turbolinks:load', function() {
+  // var CLOTHES = ["XXS以下", "XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL以上", "FREE SIZE"];
+  // var SHOES = ["20.0cm以下", "20.5cm", "21.0cm", "21.5cm", "22.0cm", "22.5cm", "23.0cm", "23.5cm", "24.0cm", "24.5cm", "25.0cm", "25.5cm", "26.0cm", "26.5cm", "27.0cm", "27.5cm以上"];
 
   function appendCategoryForm(categories, hierarchy) {
     categoryList = $('.category-block');
@@ -14,8 +16,24 @@ $(document).on('turbolinks:load', function() {
     })
     html += option;
     html += `</select>
-    <i class="fas fa-chevron-down select-arrow-append"></i>`;
+    <i class="fas fa-chevron-down select-arrow-append"></i></div>`;
     categoryList.append(html);
+  }
+
+  function appendSizeForm(sizes){
+    var sizeForm = $('#size-block');
+    var html = `<div class="form-block--select-append" id="size-form">
+    <label for="product_category_id"><p class="form-block__label">サイズ
+    <span class="form-block__require">必須</span>
+    </p></label>
+    <select class="form-block__form--select" name="product_size">
+    <option value="0">---</option>`;
+    $.each(sizes, function(i, size) {
+      html += `<option value="${size.id}">${size.text}</option>`
+    });
+    html += `</select>
+    <i class="fas fa-chevron-down select-arrow"></i></div>`;
+    sizeForm.append(html);
   }
 
   $('.category-block').on('click', function() {
@@ -67,6 +85,31 @@ $(document).on('turbolinks:load', function() {
         .fail(function() {
         })
       }
+    })
+
+    $('#low_category').off('change').on('change', function() {
+      //low_categoryが決まったときに発火
+      // low_categoryのIDから、サイズパターンのidを取得する(ajax)
+      console.log("appendsize");
+      $('#size-form').remove();
+      var lowCategory = $('#low_category').val();
+      if (lowCategory !== 0){
+        $.ajax({
+          type: 'GET',
+          url: '/product_sizes',
+          data: {low_category: lowCategory},
+          dataType: 'json'
+        })
+        .done(function(sizes) {
+          if (sizes !== 0) {
+            appendSizeForm(sizes);
+          }
+        })
+        .fail(function() {
+
+        })
+      }
+      //カテゴリーidに紐づいたサイズパターンを取得する
     })
   });
 });
