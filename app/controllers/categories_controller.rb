b4f84f9ffcb98cb89b8bffd5293f198a13eb79e9
Kohei Matsukawa <kohei.matsukawa@gmail.com>
2019-07-14 11:34:27 +0900

    カテゴリーコントローラーにnewとshowを追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category_brand, only: [:show, :new]
 
   def index
     # @categories = Category.where("category_name LIKE(?)", "#{params[:value]}")
@@ -9,4 +10,32 @@ class CategoriesController < ApplicationController
       format.json { render 'index', formats: 'json', handlers: 'jbuilder' }
     end
   end
+
+  def new
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    if @category.children.empty?
+      @item = @category
+    else
+      @children = @category.children
+    end
+
+  end
+
+
+  private
+
+  def set_category_brand
+    @brands = Brand.all
+    @parents = Category.where(ancestry: nil)
+    @parent = Category.find_by('category_name LIKE(?)', "%#{params[:keyword]}%")
+    @children = @parent.children
+    respond_to do |format|
+      format.json
+      format.html
+    end
+  end
+
 end
