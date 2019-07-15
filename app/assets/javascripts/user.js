aa572d8aeaa38e8f9ff0074686336191b926a36d
katsutoshi818 <katsutoshi8988@gmail.com>
2019-07-15 18:29:23 +0900

    mypageだけマウスオーバーできるように設定

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function() {
-  $(".down_list_right").on('mouseover',function(){
+  $(".mypage").on('mouseover',function(){
     $(".mypage-box").removeClass("none");
   });
-  $(".down_list_right").on('mouseout',function(){
+  $(".mypage").on('mouseout',function(){
     $(".mypage-box").addClass("none");
   });
 })
