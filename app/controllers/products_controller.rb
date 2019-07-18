fb0d1adb97bb8b573a1fdb694fb2303c106f231b
katsutoshi818 <katsutoshi8988@gmail.com>
2019-07-18 13:55:33 +0900

    デストロイアクションの追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -78,6 +78,12 @@ class ProductsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy if product.seller_user_id == current_user.id
+    redirect_to root_path
+  end
+
   private
   def product_params
     params.require(:product).permit(:product_name, :introduction, :product_state, :who_pays_shipping_fee, :prefecture_id, :days_to_ship, :price, :product_size_id).merge(category_id: params.require(:product)[:low_category_id], brand_id: @brand_id, seller_user_id: current_user.id, buyer_user_id: 1, trade_state: 1, way_to_ship: 1)
